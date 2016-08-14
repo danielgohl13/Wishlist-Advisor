04c6dd0521f6356cac1a2f501ef031c1a47194ad
Daniel Gohl <danielgohl13@gmail.com>
2016-08-14 17:16:57 -0400

    Adicionando interface das funções binpacking::doPacking

--- BinPacking/BinPacking.hpp
+++ BinPacking/BinPacking.hpp
@@ -9,6 +9,9 @@
     namespace binpacking {
         typedef std::vector<Game *> WishList;
         typedef std::vector<Bin *> BinCollection;
+
+        BinCollection binpacking::doPacking(WishList wl, float binSize);
+        BinCollection binpacking::doPacking(WishList wl, float binSize, BinaryTree **tree);
     }
 
 #endif
