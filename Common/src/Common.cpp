402f1b4695fbca1c9f936b3456c385ee94d2be1a
Edwino Stein <edwino.stein@gmail.com>
2016-08-14 16:38:00 -0400

    Adicionando Common-CPP

---
+++ Common/src/Common.cpp
@@ -0,0 +1,44 @@
+/*
+	Edwino Stein - 1201324411
+*/
+
+#include "../Common.hpp"
+
+int main(int argc, char *argv[]){
+
+	for(int i = 0; i < argc; i++)
+		app->setArg(i, argv[i]);
+
+	return app->main();
+}
+
+Application* Application::getInstance(){
+    static Application *instance = new Application;
+    return instance;
+}
+
+void Application::setArg(unsigned int index, std::string arg){
+
+	Application *me = Application::getInstance();
+
+	if(index >= me->args.size())
+		me->args.push_back(arg);
+
+	else
+		me->args.insert(me->args.begin() + index, arg);
+}
+
+std::string Application::getArg(unsigned int index){
+
+	Application *me = Application::getInstance();
+
+	if(index >= me->args.size())
+		return me->args.back();
+
+	else
+		return me->args[index];
+}
+
+unsigned int Application::countArgs(){
+	return Application::getInstance()->args.size();
+}
