2fe880526d2dd596362573b305d38c68d0e5de1a
Daniel Gohl <danielgohl13@gmail.com>
2016-08-14 16:48:06 -0400

    Reformulando a classe Games

---
+++ Util/src/Game.cpp
@@ -0,0 +1,36 @@
+#include "../Game.hpp"
+
+Game::Game(){}
+
+Game::Game(int order, float price, String name){
+    this->setOrder(order)
+        ->setPrice(price)
+        ->setName(name);
+}
+
+int Game::getOrder(){
+    return this->order;
+}
+
+float Game::getPrice(){
+    return this->price;
+}
+
+String Game::getName(){
+    return this->name;
+}
+
+Game *Game::setOrder(int order){
+    this->order = order;
+    return this;
+}
+
+Game *Game::setPrice(float price){
+    this->price = price;
+    return this;
+}
+
+Game *Game::setName(String name){
+    this->name = name;
+    return this;
+}
