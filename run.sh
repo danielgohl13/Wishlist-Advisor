5b5b9b9e22133eb673d1944688f3630176b31802
Edwino Stein <edwino.stein@gmail.com>
2016-08-16 23:21:18 -0400

    Melhorando o script run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,62 @@
  #!/bin/sh
 
 compiler="./tools/compiler";
-files="main.cpp BinPacking/src/Game.cpp BinPacking/src/Bin.cpp BinPacking/src/BinaryTree.cpp BinPacking/src/BinPacking.cpp";
-binary="./build/main";
-inputFile=$1;
-inputBinSize=$2;
+commonSources="BinPacking/src/Game.cpp BinPacking/src/Bin.cpp BinPacking/src/BinaryTree.cpp BinPacking/src/BinPacking.cpp";
+buildDir="./build/";
 
-bash -c "$compiler $files";
-echo "# Executando o programa...";
-echo;
-bash -c "$binary $inputFile $inputBinSize";
+function benchmark(){
+
+    if [ ! -f "${buildDir}benchmark" ] ; then
+        bash -c "$compiler benchmark.cpp  $commonSources";
+        echo "# Executando o programa...";
+        echo;
+    fi
+
+    bash -c "${buildDir}benchmark $1 $2";
+}
+
+function main(){
+    if [ ! -f "${buildDir}main" ] ; then
+        bash -c "$compiler main.cpp  $commonSources";
+        echo "# Executando o programa...";
+        echo;
+    fi
+
+    bash -c "${buildDir}main $1 $2";
+}
+
+case $1 in
+
+    "fwl")
+        bash -c "./tools/fetchWishlist $2"
+    ;;
+
+    "benchmark")
+        benchmark "$2" "$3"
+    ;;
+
+    "benchmark.")
+        benchmark "$2" "$3"
+    ;;
+
+    "benchmark.cpp")
+        benchmark "$2" "$3"
+    ;;
+
+    "main")
+        main "$2" "$3";
+    ;;
+
+    "main.")
+        main "$2" "$3";
+    ;;
+
+    "main.cpp")
+        main "$2" "$3";
+    ;;
+
+ 	*)
+ 		main "$1" "$2";
+ 	;;
+
+esac
